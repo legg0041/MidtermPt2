c7735ead14763f5b18131d7912ed37c7d4670743
Kenny Leggett <kennethleggett@gmail.com>
2016-03-07 18:01:45 -0500

    Improved main menu

---
+++ Assets/Scripts/RotateBoard.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateBoard : MonoBehaviour {
+
+    public float rotateSpeed = 25;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //rotate game board
+        transform.Rotate(0, Time.deltaTime * rotateSpeed, 0);
+	}
+}
